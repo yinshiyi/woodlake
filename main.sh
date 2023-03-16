291efa8c2e736241be7738e44c42aafbd8d1db56
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2023-03-15 23:21:58 -0700

    Create

---
+++ main.sh
@@ -0,0 +1,36 @@
+echo "# woodlake" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:yinshiyi/woodlake.git
+git push -u origin main
+
+
+https://sanmateo-ca.county-taxes.com/public/search/gsgx_property_tax?search_query=800%20N%20DELAWARE%20ST%20%23%2099
+
+140-010-020
+140-010-010
+140-010-030
+https://sanmateo-ca.county-taxes.com/public/property_tax/accounts/140-010-080
+140-010-080 804 N DELAWARE ST
+140-010-120 812 n delaware 
+https://sanmateo-ca.county-taxes.com/public/property_tax/accounts/140-010-090
+803-207	140-120-070
+845-410	
+847-401	
+902-217	
+801-307	https://www.zillow.com/homes/801-n-humboldt-st-307, /html/body/div[1]/div[6]/div[1]/div[1]/div/div/div[1]/div/div/div[1]/div[2]/div[2]/div[3]/div/div/div/ul/li[6]/div/div[2]/div/div[2]/div[2]/div/div[2]/div/div[4]/ul/li[1]/span/text()[3]
+class="Text-c11n-8-84-0__sc-aiai24-0 qxgaF"
+821-314	140-240-130
+
+800,802,814,816,818,820, n delaware st, [1-4](0[1-9]|1[0-8])
+801,803,815,817,819,821,833,835,845,847 N humboldt st
+902,904,928,930,932,934 peninusula ave
+
+https://www.zillowgroup.com/developers/api/mls-broker-data/listing-api/
+https://www.smcacre.org/assessor/assessor-maps
+https://gis.smcgov.org/gis_exchange/rastermaps/AM/BK140/14001.TIF
+https://gis.smcgov.org/gis_exchange/rastermaps/AM/BK140/14002.TIF
+https://gis.smcgov.org/gis_exchange/rastermaps/AM/BK140/14050.TIF
+140-[01-50]0-[01-28]0
